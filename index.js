51ff88f119c14edc94f5c6b3b25e9562d613f386
nailaabi <pallares.nailaabigail@iesconcepcion.edu.ar>
2025-06-11 17:58:21 -0300

    foto1

---
+++ index.js
@@ -0,0 +1,9 @@
+// crea una calculadora 
+const prompt =  require ('prompt-sync')();
+
+const num1 = Number (prompt ( 'Ingresa una numero: '))
+const num2 = Number (prompt ( 'Ingresa otronumero: '))
+
+const operacion = prompt ('ingrese una operacion '( +, -, * , / ));
+
+const
