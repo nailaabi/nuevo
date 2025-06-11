1c96509a393ec25b85dd9e4e45febb23c017d68c
nailaabi <pallares.nailaabigail@iesconcepcion.edu.ar>
2025-06-11 18:38:16 -0300

    foto3

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 // crea una calculadora 
 const prompt =  require ('prompt-sync')();
 
-const num1 = Number (prompt ( 'Ingresa una numero: '))
-const num2 = Number (prompt ( 'Ingresa otronumero: '))
+const num1 = Number (prompt ( 'Ingresa una numero: '));
+const num2 = Number (prompt ( 'Ingresa otro numero: '));
 
-const operacion = prompt ('ingrese una operacion '( +, -, * , / ));
+const operacion = prompt ('ingrese una operacion ( + , - , / , *  :)');
 
-const
+const suma = num1 + num2;
+const resta = num1 - num2;
+const division = num1 / num2;
+const multiplicacion = num1 * num2;
+
+if (operacion == ('+')){
+    console.log ( 'Este es su resultado', suma)
+
+}
+else if (operacion == ('-')){
+    console.log ( 'Este es su resultado', resta)
+
+}
+else 
+
+if (operacion == ('/')){
+    console.log ( 'Este es su resultado', division)
+
+}
+else if (operacion == ('*')){
+    console.log ( 'Este es su resultado', multiplicacion)
+
+}
